6ce97381dd85118324d2694275531fd5aa60bd9a
Paterson-Jones <rolandp@amazon.com>
2018-07-24 13:09:14 +0200

    castlerights step 1

--- types.go
+++ types.go
@@ -12,6 +12,19 @@ func oppColor(color ColorT) ColorT {
 	return Black ^ color
 }
 
+type CastleRightsT uint8
+
+const (
+	queenSideCastleRights CastleRightsT = iota
+	kingSideCastleRights
+)
+
+type CastleRightsFlagsT uint8
+
+// Flags
+const queenSideCastleRightsFlag = CastleRightsFlagsT(1) << queenSideCastleRights
+const kingSideCastleRightsFlag = CastleRightsFlagsT(1) << kingSideCastleRights
+
 // Each bitboard shall use little-endian rank-file mapping:
 // 56  57  58  59  60  61  62  63
 // 48  49  50  51  52  53  54  55
@@ -28,14 +41,11 @@ func oppColor(color ColorT) ColorT {
 // The board type, which uses little-endian rank-file mapping.
 type Board struct {
 	Colortomove ColorT
-	//Wtomove       bool
 	enpassant     uint8 // square id (16-23 or 40-47) where en passant capture is possible
-	castlerights  uint8 // TODO - make per-color array
 	Halfmoveclock uint8
+	castlerights  [NColors]CastleRightsFlagsT
 	Fullmoveno    uint16
 	Bitboards     [NColors]Bitboards // indexed by color
-	// White         Bitboards
-	// Black         Bitboards
 	pieces        [64]Piece // maps position->piece-type
 	hash          uint64
 }
@@ -127,16 +137,16 @@ func (b *Board) Enpassant() uint8 {
 
 // Castling helper functions for all 16 possible scenarios
 func (b *Board) whiteCanCastleQueenside() bool {
-	return b.castlerights&1 == 1
+	return b.castlerights[White] & queenSideCastleRightsFlag != 0
 }
 func (b *Board) whiteCanCastleKingside() bool {
-	return (b.castlerights&0x2)>>1 == 1
+	return b.castlerights[White] & kingSideCastleRightsFlag != 0
 }
 func (b *Board) blackCanCastleQueenside() bool {
-	return (b.castlerights&0x4)>>2 == 1
+	return b.castlerights[Black] & queenSideCastleRightsFlag != 0
 }
 func (b *Board) blackCanCastleKingside() bool {
-	return (b.castlerights&0x8)>>3 == 1
+	return b.castlerights[Black] & kingSideCastleRightsFlag != 0
 }
 func (b *Board) canCastleQueenside() bool {
 	if b.Colortomove == White {
@@ -167,19 +177,19 @@ func (b *Board) oppCanCastleKingside() bool {
 	}
 }
 func (b *Board) flipWhiteQueensideCastle() {
-	b.castlerights = b.castlerights ^ (1)
+	b.castlerights[White] ^= queenSideCastleRightsFlag
 	b.hash ^= castleRightsZobristC[1]
 }
 func (b *Board) flipWhiteKingsideCastle() {
-	b.castlerights = b.castlerights ^ (1 << 1)
+	b.castlerights[White] ^= kingSideCastleRightsFlag
 	b.hash ^= castleRightsZobristC[0]
 }
 func (b *Board) flipBlackQueensideCastle() {
-	b.castlerights = b.castlerights ^ (1 << 2)
+	b.castlerights[Black] ^= queenSideCastleRightsFlag
 	b.hash ^= castleRightsZobristC[3]
 }
 func (b *Board) flipBlackKingsideCastle() {
-	b.castlerights = b.castlerights ^ (1 << 3)
+	b.castlerights[Black] ^= kingSideCastleRightsFlag
 	b.hash ^= castleRightsZobristC[2]
 }
 func (b *Board) flipQueensideCastle() {
